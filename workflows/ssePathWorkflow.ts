b44fe8c61f0ab8d443f74874d8c33286d4565b57
Merkle Groot <merklegroot@gmail.com>
2025-09-24 18:43:42 -0400

    auto fail for windows

--- workflows/ssePathWorkflow.ts
+++ workflows/ssePathWorkflow.ts
@@ -3,8 +3,14 @@ import { flexibleSseHandlerProps } from "./sseFactory";
 import { SpawnResultWithParsedData } from "@/models/SpawnResult";
 import { PathResult } from "@/models/SseMessage";
 import { parsePath } from "./parsePath";
+import { platformUtil } from "@/utils/platformUtil";
 
 async function executePath(props: flexibleSseHandlerProps): Promise<SpawnResultWithParsedData<PathResult>> {
+    const platform = platformUtil.detectPlatform();
+    if (platform === 'windows') {
+        throw new Error('ssePathWorkflow is not supported on Windows');
+    }
+
     const result = await spawnAndGetDataWorkflow.execute({
         command: 'bash',
         args: ['-l', '-c', 'echo $PATH']
